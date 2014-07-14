79918df16e251f734b1f4a486d9ad31d542db78b
maany <imptodefeat@gmail.com>
2014-07-15 00:55:24 +0530

    master update

---
+++ build/classes-generated/com/jme3/gde/cinematic/Bundle.java
@@ -0,0 +1,27 @@
+package com.jme3.gde.cinematic;
+/** Localizable strings for {@link com.jme3.gde.cinematic}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>CinematicEditor</i>
+     * @see CinematicEditorTopComponent
+     */
+    static String CTL_CinematicEditorAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CinematicEditorAction");
+    }
+    /**
+     * @return <i>CinematicEditor Window</i>
+     * @see CinematicEditorTopComponent
+     */
+    static String CTL_CinematicEditorTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CinematicEditorTopComponent");
+    }
+    /**
+     * @return <i>This is a CinematicEditor window</i>
+     * @see CinematicEditorTopComponent
+     */
+    static String HINT_CinematicEditorTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CinematicEditorTopComponent");
+    }
+    private void Bundle() {}
+}
