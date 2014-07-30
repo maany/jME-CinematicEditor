cdf1fd4f188cc822697d8575f6a25fb8dd40ca01
maany <imptodefeat@gmail.com>
2014-07-30 19:02:01 +0530

    filetype init

---
+++ src/com/jme3/gde/cinematic/filetype/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Cinematic", content = "CinematicTemplate.j3c",displayName = "Cinematic Clip",description = "cinematic_editor_description.txt")
+package com.jme3.gde.cinematic.filetype;
+
+import org.netbeans.api.templates.TemplateRegistration;
